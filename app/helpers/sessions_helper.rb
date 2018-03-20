6ba49cd4f3a80fbe727954b272f1d747f2951883
Victoria Li <victoria@golantern.com>
2018-03-20 16:58:15 -0700

    Add session

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+
+  #Logs in user
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    p "logged in", current_user.present?
+    current_user.present?
+  end
+end
