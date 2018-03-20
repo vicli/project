e279e24ca11c85be612d95280e8a8d9e27c194b4
Victoria Li <victoria@golantern.com>
2018-03-19 21:48:16 -0700

    Add about route

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 RSpec.describe StaticPagesController do
   describe '.home' do
     it 'calls home route' do
+      get :home
+
+      expect(response.status).to eq(200)
     end
   end
 end
